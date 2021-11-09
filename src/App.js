71e94679255670aef8e5e1214753a719a72054ae
vijaymahar <vijaykumaranchupogu123@gmail.com>
2021-11-08 22:31:38 -0800

    old src folder added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import Body from "./body";
+import "./scss/main.css";
+const App = () => {
+  const [input, setInput] = useState("hyderabad");
+  const [totalData, setTotalData] = useState({});
+  const getData = (e) => {
+    setInput(e);
+  };
+  let submitData = async () => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=248cbc8660fdb4cc099d18b7a6920c54`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      const { temp, humidity, pressure } = data.main;
+      const { country, sunset } = data.sys;
+      const { main: weatherMood } = data.weather[0];
+      const { speed } = data.wind;
+      const totalData = {
+        temp,
+        humidity,
+        pressure,
+        country,
+        sunset,
+        weatherMood,
+        speed,
+        input,
+      };
+      setTotalData(totalData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    submitData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="application-title">weather application</h1>
+      <Container className="mt-5">
+        <center>
+          <div className="input-group">
+            <input
+              type="search"
+              placeholder="enter city.."
+              autoFocus
+              value={input}
+              onChange={(e) => getData(e.target.value)}
+            />
+            <button type="button" onClick={submitData}>
+              search
+            </button>
+          </div>
+        </center>
+      </Container>
+      <Body totalData={totalData} />
+    </>
   );
-}
+};
 
 export default App;
